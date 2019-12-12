db581af10bbeef451257b176a485febc57483cc6
Matheus Gustavo <matheus_guga2012@hotmail.com>
2019-12-12 13:58:35 -0300

    Adicionando botões e imagens de categorias

--- src/app/categorias/categorias.page.ts
+++ src/app/categorias/categorias.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { CategoriaService } from 'src/services/domain/categoria.service';
+import { CategoriaDTO } from 'src/models/categoria.dto';
+import { API_CONFIG } from 'src/config/api.config';
 
 @Component({
   selector: 'app-categorias',
@@ -8,6 +10,9 @@ import { CategoriaService } from 'src/services/domain/categoria.service';
   styleUrls: ['./categorias.page.scss'],
 })
 export class CategoriasPage {
+
+  items: CategoriaDTO[];
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
 
   constructor(
     private categoriaService: CategoriaService,
@@ -26,7 +31,7 @@ export class CategoriasPage {
    public findAll() {
     this.categoriaService.findAll()
     .subscribe(response => {
-      console.log(response);
+      this.items = response;
     },
     error => {
       console.log(error);
--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://127.0.0.1:8080"
+    baseUrl: "http://127.0.0.1:8080",
+    bucketBaseUrl:"https://curso-spring-ionic-matheus-gustavo.s3-sa-east-1.amazonaws.com"
 }
